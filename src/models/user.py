be05da86613adf41673c56e187b4d2fd9f7bccd1
Beverly Pham <beverly.pham@gmail.com>
2019-04-15 11:30:14 -0700

    get user date by email or id

--- src/models/user.py
+++ src/models/user.py
@@ -1,16 +1,26 @@
 """User model."""
+import uuid
+from src.common.database import Database
 
 
 class User:
-    def __init__(self, email, password):
+    def __init__(self, email, password, _id=None):
         self.email = email
         self.password = password
+        self._id = uuid4().hex if not _id else _id
 
-    def get_by_email(self):
-        pass
+    @classmethod
+    def get_by_email(cls, email):
+        """Find if user exists in database."""
+        data = Database.find_one('users', {'email': self.email})
+        if data:
+            return cls(**data)
 
-    def get_by_id(self):
-        pass
+    @classmethod
+    def get_by_id(cls, _id):
+        data = Database.find_one('users', {'_id': self._id})
+        if data:
+            return cls(**data)
 
     def login_valid(self):
         pass
