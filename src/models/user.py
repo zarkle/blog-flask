f1108971ef8676f8c8bc5fbd11544dfa26efb067
Beverly Pham <beverly.pham@gmail.com>
2019-04-15 11:25:52 -0700

    scaffold out user model class

---
+++ src/models/user.py
@@ -0,0 +1,24 @@
+"""User model."""
+
+
+class User:
+    def __init__(self):
+        pass
+
+    def get_by_email(self):
+        pass
+
+    def get_by_id(self):
+        pass
+
+    def login_valid(self):
+        pass
+
+    def registerself):
+        pass
+
+    def login(self):
+        pass
+
+    def get_blogs(self):
+        pass
