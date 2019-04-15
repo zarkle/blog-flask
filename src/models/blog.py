ae3f75819e8f7a7b5fff2d616eabec57ff9c1d88
Beverly Pham <beverly.pham@gmail.com>
2019-04-15 10:56:47 -0700

    overwrite default mongo id with our own id

--- src/models/blog.py
+++ src/models/blog.py
@@ -1,10 +1,10 @@
 """Blog class."""
-from uuid import uuid4
+from uu_id import uuid4
 from datetime import datetime
 from post import Post
 from database import Database
 
-blog = Blog(autho='Joe', title='Some title', description='stuff and things')
+blog = Blog(author='Joe', title='Some title', description='stuff and things')
 
 blog.new_post()
 
@@ -16,11 +16,11 @@ blog.get_posts()
 
 
 class Blog():
-    def __init__(self, author, title, description, id=None):
+    def __init__(self, author, title, description, _id=None):
         self.author = author
         self.title = title
         self.description = description
-        self.id = uuid4().hex if id is None else id
+        self._id = uu_id4().hex if _id is None else _id
 
     def new_post(self):
         title = input('Enter post title: ')
@@ -30,7 +30,7 @@ class Blog():
             date = datetime.datetime.utcnow()
         else:
             datetime.strptime(date, '%d%m%Y')
-        post = Post(blog_id=self.id,
+        post = Post(blog__id=self._id,
                     title=title,
                     content=content,
                     author=self.author
@@ -38,7 +38,7 @@ class Blog():
         post.save_to_mongo()
 
     def get_posts(self):
-        return Post.from_blog(self.id)
+        return Post.from_blog(self._id)
 
     def save_to_mongo(self):
         Database.insert(collection='blogs', data=self.json())
@@ -48,13 +48,13 @@ class Blog():
             'author': self.author,
             'title': self.title,
             'description': self.description,
-            'id': self.id
+            '_id': self._id
         }
 
     @classmethod
     def from_mongo(cls, id):
-        blog_data = Database.find_one(collection='blogs', query={'id': id})
+        blog_data = Database.find_one(collection='blogs', query={'_id': id})
         return cls(author=blog_data['author'],
                     title=blog_data[''],
                     description=blog_data['description'],
-                    id=blog_data['id'])
+                    _id=blog_data['_id'])
