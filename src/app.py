55c3605880be17a1b7634095cf779b24c077a498
Beverly Pham <beverly.pham@gmail.com>
2019-04-15 23:48:08 -0700

    make login route

--- src/app.py
+++ src/app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, session
+from src.models.user import User
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
     return render_template('login.html')
+
+@app.route('/login')
+def login_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    if User.login_valid(email, password):
+        User.login(email)
+
+    return render_template('profile.html', email=session['email'])
 
 if __name__ == "__main__":
     app.run()
